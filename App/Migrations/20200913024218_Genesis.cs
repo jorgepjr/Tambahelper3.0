c8693f2018cb6896f3cb93b369322127058640e0
Jorge <jorgepaiva1806@gmail.com>
2020-09-12 22:53:38 -0400

    migrations

---
+++ App/Migrations/20200913024218_Genesis.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace App.Migrations
+{
+    public partial class Genesis : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tecnico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnico", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Senha = table.Column<string>(maxLength: 36, nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    Hash = table.Column<string>(nullable: true),
+                    HashUtilizado = table.Column<bool>(nullable: false),
+                    Perfil = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Help",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TipoDeProblema = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Setor = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    TecnicoId = table.Column<int>(nullable: true),
+                    InicioDoAtendimento = table.Column<DateTime>(nullable: false),
+                    FimDoAtendimento = table.Column<DateTime>(nullable: false),
+                    Situacao = table.Column<int>(nullable: false),
+                    DataDeRegistro = table.Column<DateTime>(nullable: false),
+                    Solucao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Help", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Help_Tecnico_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnico",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Help_TecnicoId",
+                table: "Help",
+                column: "TecnicoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Help");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Tecnico");
+        }
+    }
+}
